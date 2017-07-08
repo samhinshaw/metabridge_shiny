63e9c20a60049037cd93b51bfe787c5ea5c4dc81
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-07-07 20:13:52 -0700

    problem: when re-uploading same file, no change is detected. solution: make file reading observeEvent dependent on the header & sep inputs as well
    also: added another dummy dataset to test with

--- server.R
+++ server.R
@@ -25,29 +25,36 @@ shinyServer(function(input, output) {
   ################################################
   
   ## Inject example df when "Try Examples" selected
-  eventReactive(input$tryExamples, {
+  observeEvent(input$tryExamples, {
+    # print(name_map)
     metaboliteObject(name_map)
   })
   
   ## Placeholder CSV handling
-  observeEvent(input$metaboliteUpload, {
-    read_delim(file = input$metaboliteUpload$datapath, 
-               col_names = input$header, delim = input$sep) %>% 
-      metaboliteObject()
-  }, ignoreNULL = TRUE)
+  observeEvent({
+    input$metaboliteUpload
+    # make sure this is reevaluated if the sep or header changes
+    input$sep
+    input$header
+    }, {read_delim(file = input$metaboliteUpload$datapath, 
+                   col_names = input$header, delim = input$sep) %>% 
+        metaboliteObject()
+  }, ignoreNULL = TRUE, ignoreInit = TRUE)
   
-  output$contents <- renderTable({
+  output$uploadSuccess <- renderText({
+    input$tryExamples # make sure the try examples button is a dependency
+    if (is.null(metaboliteObject()))
+      return(NULL)
+    paste0("Check below to see that your data has been uploaded properly.  ",
+           "If so, continue to the 'Plot' tab!")
+  })
+  
+  output$uploadedDataTable <- renderTable({
     input$tryExamples # make sure the try examples button is a dependency
     if (is.null(metaboliteObject()))
       return(NULL)
     metaboliteObject()
   })
-  
-  output$diagnostics <- renderText({
-    print(input$tryExamples[1]) # make sure the try examples button is a dependency
-    metaboliteObject() %>% class()
-  })
-  
   
   # Placeholder output for Output Tab
   output$databases <- renderTable({
