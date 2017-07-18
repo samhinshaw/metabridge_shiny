ec2cc103bf3b63c42d1a3409571d0d2b1cd0fc76
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-07-18 13:42:57 -0700

    use tryCatch to improve error handling

--- mapGenerally.R
+++ mapGenerally.R
@@ -12,42 +12,101 @@ mapMetaCyc <- function(importDF, col, idType) {
   #                     #
   #######################
   
-  ## Suppress Errors for this chunk, because we already know that there may be
-  ## some mapping issues cropping up with our joins.
+  ## Let's attempt using tryCatch to handle errors for each joining step.
   
-  suppressWarnings({
     # Extract the column of interest (col) and construct a new tibble with which 
     # we will mape our metabolites to enzymes and then genes. As character just in
     # case IDs such as PubChem IDs are interpreted as integers
+  mappingDF <- tryCatch({
     mappingDF <- data_frame(UQ(col) := importDF %>% use_series(UQ(col)) %>% as.character())
-
-        # Now map these to the MetaCyc Object IDs
-    mappedDF <- inner_join(mappingDF, metaCycDBLinks, by = setNames(nm = c(col = idType))) %>% 
-      select_(col, "Compound") %>% rename_("compound" = "Compound")
-    
-    # Finally, join the reaction-gene table!
-    
-    fullyMappedDF <- inner_join(mappedDF, metaCycDB, by = "compound")
-    
-    
-    ## Finally, finally, map biocyc gene IDs to ensembl gene IDs
-    
-    mappedDFEnsembl <- inner_join(fullyMappedDF, metaCycGeneIDs, 
-                                  by = c("gene" = "Object ID"))
+  }, warning = function(warningMessage) {
+    warningMessage
+  }, error = function(errorMessage) {
+    errorMessage
+  }, finally = {
+    mappingDF
   })
   
+  # If exited with error, return previous step with relevant error message
+  if (!is.data.frame(mappingDF)) {
+    mappingAlert('We were unable to properly import your data.', paste0(mappingDF))
+    return(importDF)
+  }
+  if (nrow(mappingDF) == 0) {
+    mappingAlert('We were unable to properly import your data.', paste0(mappingDF))
+    return(importDF)
+  }
   
+  # Now map these to the MetaCyc Object IDs
+  mappedDF <- tryCatch({
+    mappedDF <- inner_join(mappingDF, metaCycDBLinks, by = setNames(nm = c(col = idType))) %>% 
+      select_(col, "Compound") %>% rename_("compound" = "Compound")
+  }, warning = function(warningMessage) {
+    warningMessage
+  }, error = function(errorMessage) {
+    errorMessage
+  }, finally = {
+    mappedDF
+  })
   
-  ## Now we step up the chian!
-  if (nrow(mappedDFEnsembl) != 0) {
-    return(mappedDFEnsembl)
-  } else if (nrow(fullyMappedDF) != 0) {
-    return(fullyMappedDF)
-  } else if (nrow(mappedDF) != 0) {
-    return(mappedDF)
-  } else if (nrow(mappingDF) != 0) {
+  # If exited with error, return previous step with relevant error message
+  if (!is.data.frame(mappedDF)) {
+    mappingAlert('We were unable to map your metabolites to MetaCyc Compound IDs.', paste0(mappedDF))
     return(mappingDF)
   }
+  if (nrow(mappedDF) == 0) {
+    mappingAlert(paste0('We were unable to find any matches in the MetaCyc ',
+                        'database for the compound IDs you provided.'), paste0(mappedDF))
+    # cat(mappedDF)
+    return(mappingDF)
+  }
+  
+  # Finally, join the reaction-gene table!
+  fullyMappedDF <- tryCatch({
+    fullyMappedDF <- inner_join(mappedDF, metaCycDB, by = "compound")
+  }, warning = function(w) {
+    'error'
+  }, error = function(e) {
+    'error'
+  }, finally = {
+    fullyMappedDF
+  })
+  
+  # If exited with error, return previous step with relevant error message
+  if (!is.data.frame(fullyMappedDF)) {
+    mappingAlert('We were unable to map your compounds to any reactions.', paste0(fullyMappedDF))
+    return(mappedDF)
+  }
+  if (nrow(fullyMappedDF) == 0) {
+    mappingAlert('We were unable to map your compounds to any reactions.', paste0(fullyMappedDF))
+    return(mappedDF)
+  }
+  
+    ## Finally, finally, map biocyc gene IDs to ensembl gene IDs
+  mappedDFEnsembl <- tryCatch({
+    mappedDFEnsembl <- inner_join(fullyMappedDF, metaCycGeneIDs, 
+                                  by = c("gene" = "Object ID"))
+  }, warning = function(warningMessage) {
+    warningMessage
+  }, error = function(errorMessage) {
+    errorMessage
+  }, finally = {
+    mappedDFEnsembl
+  })
+  
+  # If exited with error, return previous step with relevant error message
+  if (!is.data.frame(mappedDFEnsembl)) {
+    mappingAlert('There was an error mapping your compounds to human gene IDs.', paste0(mappedDFEnsembl))
+    return(fullyMappedDF)
+  }
+  if (nrow(mappedDFEnsembl) == 0) {
+    mappingAlert(paste0('We were unable to find any matches for ',
+                        'human gene IDs given the enzymes mapped.'), paste0(mappedDFEnsembl))
+    return(fullyMappedDF)
+  }
+  
+  # print('Returning Final Step Mapped DF')
+  return(mappedDFEnsembl)
 }
 
 mapKEGG <- function(importDF, col, idType) {
@@ -63,30 +122,41 @@ mapKEGG <- function(importDF, col, idType) {
   # map the given IDs to their KEGG compound IDs
   
   if (idType != "KEGG") {
-    suppressWarnings({
-      keggIDs <- metaCycDBLinks %>%
-        dplyr::filter_(paste0(col, " %in% importedVector")) %>% ## This should use rlang
-        magrittr::extract2("KEGG") %>% unique()
-      keggIDsToMap <- keggIDs
-    })
+    keggIDs <- metaCycDBLinks %>%
+      ## This `filter_()` should use rlang
+      dplyr::filter_(paste0(col, " %in% importedVector")) %>% 
+      magrittr::extract2("KEGG") %>% unique()
+    keggIDsToMap <- keggIDs
   } else if (idType == "KEGG") {
     keggIDsToMap <- importedVector
   } else {
-    stop("Something went wrong when mapping against the KEGG database Probably an error with the idType parameter.")
+    alert("Something went wrong when mapping against the KEGG database.
+          Probably an error with the idType parameter.")
   }
-  suppressWarnings({
-    keggGenesOfInterest <- keggDB %>%
-      dplyr::filter_("KEGG %in% importedVector")
-  })
+  
+  keggGenesOfInterest <- keggDB %>%
+    dplyr::filter_("KEGG %in% importedVector") %>% 
+    rename_('Enzyme' = 'enzymes', 'Compound' = 'KEGG', 'Gene' = 'genes') %>% 
+    mutate(Compound = paste0('<a target="_blank" href="http://www.genome.jp/dbget-bin/www_bget?cpd:',
+                             Compound, '">', Compound, '</a>')) %>% 
+    mutate(Enzyme = paste0('<a target="_blank" href="http://www.genome.jp/dbget-bin/www_bget?ec:', 
+                           Enzyme, '">', Enzyme, '</a>'))
   
   if (nrow(keggGenesOfInterest) != 0) {
     return(keggGenesOfInterest)
   } else if (nrow(keggIDsToMap) != 0) {
     return(keggIDsToMap)
   } else {
-    return(importDF %>% magrittr::use_series(col))
+    return(importDF %>% extract2(col))
   }
 }
+
+
+#######################################################
+#                                                     #
+#        Map Generally Against EitheE Database        #
+#                                                     #
+#######################################################
 
 mapGenerally <- function(importDF, col, db, idType) {
   if (db == "KEGG") {
@@ -94,7 +164,8 @@ mapGenerally <- function(importDF, col, db, idType) {
   } else if (db == "MetaCyc") {
     mappedMetabolites <- mapMetaCyc(importDF = importDF, col = col, idType = idType)
   } else {
-    stop("Something went wrong when mapping generally. Probably an error with the database parameter.")
+    alert("Something went wrong when mapping generally
+          Probably an error with the database parameter.")
   }
   return(mappedMetabolites)
 }
