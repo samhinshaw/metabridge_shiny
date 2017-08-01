933834c92e049a256d83fb22a6d4c50d0e1c3256
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-08-01 15:51:03 -0700

    reencode data from CSV to RData objects for faster IO and smaller filesize. add lists of human compounds, genes, reactions, pathways, for filtering of MetaCyc data

--- global.R
+++ global.R
@@ -9,18 +9,30 @@ library(rlang)
 library(tools)
 library(pathview)
 
-## Load System Data
+# Load System Data
+# This has since been re-saved to individual files.
 load('data/sysdata.rda')
 
-##### Read In Example KEGG Pathways ######
-load('data/examplePathways.RData') 
-# Will load full pathway set here
-##########################################
-
+# Load Example Data
 load('data/name_map.RData')
-load(file = 'data/reactions_pathways_readable.RData')
-load(file = 'data/reactions_pathways.RData')
 
+# Load KEGG Database Files
+# Import Enzymes & Enzyme Names here once scraping has been completed.
+load('data/k01_keggEnzymes.RData')
+load('data/k03_keggPathways.RData')
+
+# Load MetaCyc Database Files
+load('data/m01_metaCycDBLinks.RData')
+load('data/m02_metaCycDB.RData')
+load('data/m03_metaCycGeneIDs.RData')
+load('data/m04_metaCycPathways.RData')
+
+# Load HumanCyc Cross-References
+load('data/humanRefs.RData')
+
+# Source Mapping Functions
 source('mapGenerally.R', local = TRUE)$value
 source('pathwayMapping.R', local = TRUE)$value
+
+# Source App Functions
 source('alertFunctions.R', local = TRUE)$value
