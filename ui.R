04205fed06f615ae2d0dc8ddcad8c358da1aea75
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-08-17 19:10:06 -0700

    adding development warning to dev branch

--- ui.R
+++ ui.R
@@ -19,6 +19,16 @@ shinyUI(fluidPage(
     collapsible = TRUE,
     tabPanel("Welcome", value = "welcomePanel",
              tags$div(
+               id = 'mappingAlert',
+               class = "alert alert-dismissible alert-danger",
+               tags$button(
+                 HTML("&times;"),
+                 type = "button",
+                 class = "close",
+                 `data-dismiss` = "alert"),
+               "Warning: In development environment!"
+             ),
+             tags$div(
                class = "jumbotron",
                h1("Welcome"),
                tags$p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
