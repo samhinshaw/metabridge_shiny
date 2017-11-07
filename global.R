01e64fc85d25d69e3d344dac1359613ce960f8ca
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-11-06 16:40:34 -0800

    Further munging of MetaCyc SmartTables

--- global.R
+++ global.R
@@ -1,3 +1,6 @@
 ## Load Packages
 library(shiny)
 library(shinyjs)
+
+## If you are looking for the rest of the app initialization code, they are
+## lazy-loaded! Look in deferred.R, which is called after shiny initialization.
