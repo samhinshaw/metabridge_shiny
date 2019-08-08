f079d15bf6f51a4fe1098e7887543b9350fb0166
Travis <travis.m.blimkie@gmail.com>
2019-08-08 13:42:58 -0700

    Added and filled out comment skeleton for function.

--- functions/visualizePathways.R
+++ functions/visualizePathways.R
@@ -1,6 +1,20 @@
+#' visualizePathview
+#'
+#' @param pathway
+#' @param genes
+#' @param cpd
+#'
+#' @return filename
+#' @export
+#'
+#' @examples
+#'
+#' Given a selected pathway, calls pathview to show the enzyme/metabolite
+#' interactions involving the selected genes
+#'
 visualizePathview <- function(pathway, genes, cpd) {
 
-  # Generate the PNG
+  # Generate the PNG of desired pathway, and suppress messages from pathview()
   suppressWarnings({
     pathview(
       gene.data = genes,
