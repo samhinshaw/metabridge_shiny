fe957d6d8d900305bb654c5e61858ad03ead461d
Travis <travis.m.blimkie@gmail.com>
2019-08-08 12:09:01 -0700

    Added roxygen skeletons to described utility functions.

--- functions/utilityFunctions.R
+++ functions/utilityFunctions.R
@@ -1,22 +1,59 @@
+
+#' notNAs
+#'
+#' @param vector
+#'
+#' @return vector
+#' @export
+#'
+#' @examples
+#'
+#' Simple function to remove NA values from input vector
+#'
 notNAs <- function(vector) {
   vector <- vector[!is.na(vector)]
   return(vector)
 }
 
+#' notEmpty
+#'
+#' @param vector
+#'
+#' @return vector
+#' @export
+#'
+#' @examples
+#'
+#' Simple function to remove empty elements from a vector
+#'
 notEmpty <- function(vector) {
   vector <- vector[!grepl(x = vector, pattern = "^$")]
   return(vector)
 }
 
-# MetaCyc only supports the older format, 5 digit HMDB IDs. If we detect your
-# HMDB IDs are in the newer 7 digit formar, we will trim the leading characters
-# if they are zeros. If they are not zeros, we will return an error.
+
+#' matchHMDB
+#'
+#' @param hmdbID
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' MetaCyc only supports the older format, 5 digit HMDB IDs. If we detect your
+#' HMDB IDs are in the newer 7 digit formar, we will trim the leading characters
+#' if they are zeros. If they are not zeros, we will return an error.
+#'
 matchHMDB <- function(hmdbID) {
+
   # Make sure the ID is a character amd starts with 'HMDB' or 'hmdb'
   # Look at the syntax very carefully here, the parens are IMPORTANT
+
   if (!is.character(hmdbID) | !(str_detect(hmdbID, "^HMDB") | str_detect(hmdbID, "^hmdb"))) {
     return(NA)
-    # If the ID is in the new, 7 digit format, check the leading digits
+
+  # If the ID is in the new, 7 digit format, check the leading digits
   } else if (nchar(hmdbID) == 11) {
     # If the leading characters are 00, simply trim the string
     if (str_sub(hmdbID, start = 5, end = 6) == "00") {
@@ -26,12 +63,14 @@ matchHMDB <- function(hmdbID) {
     } else {
       return(NA)
     }
-    # Otherwise, if the ID is in the older, 5-digit format, simply return the ID as-is.
+
+  # Otherwise, if the ID is in the older, 5-digit format, simply return the ID as-is.
   } else if (nchar(hmdbID) == 9) {
     # Do this **anyways** because it'll ensure we have capital letters at the start of the ID
     newID <- paste0("HMDB", str_sub(hmdbID, start = -5, end = -1))
     return(newID)
-    # If there is an edge case where the ID is not 9 or 11 charactesr in length(), also return NA
+
+  # If there is an edge case where the ID is not 9 or 11 charactesr in length(), also return NA
   } else {
     return(NA)
   }
